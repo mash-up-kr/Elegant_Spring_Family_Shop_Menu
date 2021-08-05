e5570d9e73756b31cd595b58f5662d33da2b9c75
Gyunny <wjdrbs966@naver.com>
2021-08-05 22:09:43 +0900

    feature: project init

---
+++ src/main/kotlin/com/mashup/shopmenu/ShopMenuApplication.kt
@@ -0,0 +1,11 @@
+package com.mashup.shopmenu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopMenuApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShopMenuApplication>(*args)
+}
---
+++ src/test/kotlin/com/mashup/shopmenu/ShopMenuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mashup.shopmenu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopMenuApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
